42df49a3e2c2ac9cb6308c0519ef4fe97323539a
elainexing <elainexing99@gmail.com>
2025-09-08 10:32:39 -0400

    added class Multiples

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,23 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples(1000, 3, 5);
+
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+
+        int i;
+        for(i = 1; i < n; i++) {
+            boolean divisibleBya = i % a == 0;
+            boolean divisibleByb = i % b == 0;
+            if(divisibleBya || divisibleByb) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
